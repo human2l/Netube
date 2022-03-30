588488322a515794cf0110059f7d77ed2501ec75
Kai <human2lkai@gmail.com>
2022-03-30 13:43:37 +1100

    Add Modal component
    use node package: react-modal

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,8 +1,24 @@
 import { useRouter } from "next/router";
-
+import Modal from "react-modal";
+import styles from "../../styles/Video.module.css";
+Modal.setAppElement("#__next");
 const Video = () => {
   const router = useRouter();
 
-  return <div>{router.query.videoId}</div>;
+  return (
+    <div>
+      {router.query.videoId}
+      <Modal
+        isOpen={true}
+        onRequestClose={() => {
+          router.back();
+        }}
+        overlayClassName={styles.overlay}
+        contentLabel="Watch the video"
+      >
+        <div>Modal body</div>
+      </Modal>
+    </div>
+  );
 };
 export default Video;
