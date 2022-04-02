bd31c64d6cc7605facb3d0115fe5a843c55a4730
Kai <human2lkai@gmail.com>
2022-04-02 14:28:39 +1100

    Add jwt generation code

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,12 +1,31 @@
 import { magicAdmin } from "../../lib/magic";
+import jwt from "jsonwebtoken";
 
 const login = async (req, res) => {
   if (req.method === "POST") {
     try {
       const auth = req.headers.authorization;
       const didToken = auth ? auth.substr(7) : "";
+      // invoke Magic
       const metadata = await magicAdmin.users.getMetadataByToken(didToken);
       console.log({ metadata });
+
+      // create jwt
+
+      const token = jwt.sign(
+        {
+          ...metadata,
+          iat: Math.floor(Date.now() / 1000),
+          exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
+          "https://hasura.io/jwt/claims": {
+            "x-hasura-allowed-roles": ["user", "admin"],
+            "x-hasura-default-role": "user",
+            "x-hasura-user-id": `${metadata.issuer}`,
+          },
+        },
+        process.env.JWT_SECRET_KEY
+      );
+      console.log({ token });
 
       return res.send({ done: true });
     } catch (error) {
