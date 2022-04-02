978229fbdeaf65501fc60a42737264e3888e3082
Kai <human2lkai@gmail.com>
2022-04-02 13:14:14 +1100

    server side Magic Infrastructure

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,6 +1,13 @@
+import { magicAdmin } from "../../lib/magic";
+
 const login = async (req, res) => {
   if (req.method === "POST") {
     try {
+      const auth = req.headers.authorization;
+      const didToken = auth ? auth.substr(7) : "";
+      const metadata = await magicAdmin.users.getMetadataByToken(didToken);
+      console.log({ metadata });
+
       return res.send({ done: true });
     } catch (error) {
       console.log("Something went wrong logging in", error);
