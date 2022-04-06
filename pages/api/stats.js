c5bc67153f83eab89e686ac9a3097bb588426fbf
Kai <human2lkai@gmail.com>
2022-04-06 19:28:52 +1000

    Update stats.js
    Add token check code

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,6 +1,14 @@
 const stats = async (req, res) => {
   if (req.method === "POST") {
-    res.send({ message: "works" });
+    try {
+      if (!req.cookies.token) {
+        return res.status(403).send({});
+      }
+      res.send({ message: "works" });
+    } catch (error) {
+      console.error("Error occurred /stats", error);
+      res.status(500).send({ done: false, error: error?.message });
+    }
   }
 };
 export default stats;
