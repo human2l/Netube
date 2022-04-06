07b9e28c62542b93fb84fff279de3255481567a2
Kai <human2lkai@gmail.com>
2022-04-06 19:32:53 +1000

    Update stats.js
    Add logic to decode jwt

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,10 +1,14 @@
+import jwt from "jsonwebtoken";
 const stats = async (req, res) => {
   if (req.method === "POST") {
     try {
-      if (!req.cookies.token) {
+      const token = req.cookies.token;
+      if (!token) {
         return res.status(403).send({});
       }
-      res.send({ message: "works" });
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log({ decoded });
+      res.send({ message: "works", decoded });
     } catch (error) {
       console.error("Error occurred /stats", error);
       res.status(500).send({ done: false, error: error?.message });
