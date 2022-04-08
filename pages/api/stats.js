8ab145fe61f61d5ec1dac0fc71ad83f6c699965f
Kai <human2lkai@gmail.com>
2022-04-08 16:32:41 +1000

    api /stats: use query string for video ID

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -4,21 +4,21 @@ import { findVideoByUserIdAndVideoId } from "../../lib/db/hasura";
 const stats = async (req, res) => {
   if (req.method === "POST") {
     try {
+      const videoId = req.query.videoId;
       const token = req.cookies.token;
       if (!token) {
         return res.status(403).send({});
       }
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      console.log({ decoded });
-      const userId = `did:ethr:0xCdfe8EF9086736823e3fe751F4239264c459B74a`;
-      const videoId = "4zH5iYM4wJo";
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+      console.log({ decodedToken });
+      const userId = decodedToken.issuer;
       const findVideoId = await findVideoByUserIdAndVideoId(
         userId,
         videoId,
         token
       );
 
-      res.send({ message: "works", decoded, findVideoId });
+      res.send({ message: "works", decodedToken, findVideoId });
     } catch (error) {
       console.error("Error occurred /stats", error);
       res.status(500).send({ done: false, error: error?.message });
