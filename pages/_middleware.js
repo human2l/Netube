3ae2fcbffffcf2a47ba722104de2526457d03d97
Kai <human2lkai@gmail.com>
2022-04-12 15:08:01 +1000

    Update _middleware.js

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,12 +1,10 @@
 import { NextResponse } from "next/server";
-import { verifyToken } from "../lib/utils";
 
 export async function middleware(req, ev) {
   const token = req.cookies.token;
-  const userId = await verifyToken(token);
   if (
     // if auth or static/login path
-    userId ||
+    token ||
     req.nextUrl.pathname.includes("/login") ||
     req.nextUrl.pathname.includes("/static")
   ) {
